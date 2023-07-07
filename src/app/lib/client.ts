b64633f7df6e1aff5f85441b62bf19e5e394a144
h662Olbm <h662@olbm.app>
2023-07-07 13:48:33 +0900

    please deploy

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,6 +1,6 @@
-"use client";
-
 import Web3, { Personal } from "web3";
 
-export const web3 = new Web3(window.ethereum);
-export const personal = new Personal(window.ethereum);
+export const web3 = new Web3(typeof window !== "undefined" && window.ethereum);
+export const personal = new Personal(
+  typeof window !== "undefined" && window.ethereum
+);
