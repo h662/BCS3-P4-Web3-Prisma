13a62b23e526fcfb3be1b4810cbb455d288f633e
h662Olbm <h662@olbm.app>
2023-07-07 13:57:51 +0900

    please deploy 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { FormEventHandler, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Web3, { Personal } from "web3";
 
-const web3 = new Web3(window.ethereum);
-const personal = new Personal(window.ethereum);
+const web3 = new Web3(typeof window !== "undefined" && window.ethereum);
+const personal = new Personal(typeof window !== "undefined" && window.ethereum);
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState<string>("");
