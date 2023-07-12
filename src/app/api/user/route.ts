144e96e43493c0125328a8b09a91a623de030d79
h662Olbm <h662@olbm.app>
2023-07-12 09:04:13 +0900

    bcs3

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,38 @@
 import { prisma } from "@/app/lib/server";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const signedToken = searchParams.get("signed-token");
+
+    const user = await prisma.user.findFirst({
+      where: {
+        signedToken,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          ok: false,
+          error: "Not exist token.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return NextResponse.json({
+      ok: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
